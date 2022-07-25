505a63a7162e59a8934c70e4e6da3808048ef896
canhlocphan <canhloc.phan@m.citynow.vn>
2022-07-25 18:11:16 +0700

    style for menu item

--- src/components/MainSideMenu/components/MenuItem/index.jsx
+++ src/components/MainSideMenu/components/MenuItem/index.jsx
@@ -1,13 +1,15 @@
 // libs
 import React from "react";
-import {UnorderedListOutlined} from "@ant-design/icons"
+import { UnorderedListOutlined } from "@ant-design/icons";
 // others
 import "./styles.scss";
 
 const MenuItem = ({ title }) => (
   <div className="menu-item-wrapper">
-    <UnorderedListOutlined />
-    <div className="menu-item-inner">{title}</div>
+    <div className="menu-item-icon-wrapper">
+      <UnorderedListOutlined className="menu-item-icon"/>
+    </div>
+    <div className="menu-item-title">{title}</div>
   </div>
 );
 
