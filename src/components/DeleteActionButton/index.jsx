f15a2ee76e2c5e0118da97fe95221582ab22326b
canhlocphan <canhloc.phan@m.citynow.vn>
2022-07-28 14:51:00 +0700

    add Popconfirm

--- src/components/DeleteActionButton/index.jsx
+++ src/components/DeleteActionButton/index.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { DeleteOutlined } from "@ant-design/icons";
 import { useDispatch } from "react-redux";
+import { Popconfirm } from "antd";
 // slices
 import { deleteCategorie } from "@/slices/categoriesSlices";
 // others
@@ -10,14 +11,17 @@ import "./styles.scss";
 const DeleteActionButton = ({ record }) => {
   const dispatch = useDispatch();
   return (
-    <div>
-      <div
-        className="delete-action-button-wrapper"
-        onClick={() => dispatch(deleteCategorie(record))}
-      >
+    <Popconfirm
+      placement="bottom"
+      title="Are you sure you want to delete item?"
+      onConfirm={() => dispatch(deleteCategorie(record))}
+      okText="Yes"
+      cancelText="No"
+    >
+      <div className="delete-action-button-wrapper">
         <DeleteOutlined />
       </div>
-    </div>
+    </Popconfirm>
   );
 };
 
