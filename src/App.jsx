dfa203de4610ce62df47c7d16d7db5b7f5c3da1a
Loc-Phan <canhloc.phan@m.citynow.vn>
2022-07-30 15:19:52 +0700

    add content wrapper for body

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // libs
 import { Routes, Route } from "react-router-dom";
 // components
+import ContentContainer from "./components/ContentContainer";
 import MainSideMenu from "./components/MainSideMenu";
 import CategorieList from "./pages/CategorieList";
 import OrderList from "./pages/OrderList";
@@ -12,10 +13,12 @@ const App = () => (
   <div className="app-wrapper">
     <div className="app-wrapper-inner">
       <MainSideMenu />
-      <Routes>
-        <Route path="/" element={<CategorieList />} />
-        <Route path="/orders-list" element={<OrderList />} />
-      </Routes>
+      <ContentContainer>
+        <Routes>
+          <Route path="/" element={<CategorieList />} />
+          <Route path="/orders-list" element={<OrderList />} />
+        </Routes>
+      </ContentContainer>
     </div>
   </div>
 );
--- src/pages/OrderList/index.jsx
+++ src/pages/OrderList/index.jsx
@@ -3,8 +3,6 @@ import React from "react";
 // components
 import OrderListHeader from "./mains/OrderListHeader";
 import OrderListContent from "./mains/OrderListContent";
-// others
-import "./styles.scss";
 
 const OrderList = () => (
   <div className="order-list-wrapper">
